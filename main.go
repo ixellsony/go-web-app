f9c2907aaee54e62f82a2d19d19322d565eeebb7
ixellsony <88244854+ixellsony@users.noreply.github.com>
2024-08-12 10:42:51 +0200

    Add files via upload

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", serveIndex)
+	http.HandleFunc("/view/", serveView)
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func serveIndex(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
+}
+
+func serveView(w http.ResponseWriter, r *http.Request) {
+	viewName := r.URL.Path[len("/view/"):]
+
+	if viewName == "" {
+		http.NotFound(w, r)
+		return
+	}
+
+	tmpl, err := template.ParseFiles("views/" + viewName + ".html")
+	if err != nil {
+		http.Error(w, "View not found", http.StatusNotFound)
+		return
+	}
+
+	// Extraire les paramètres de la requête
+	params := r.URL.Query()
+
+	// Transmettre les paramètres au template
+	tmpl.Execute(w, params)
+}
